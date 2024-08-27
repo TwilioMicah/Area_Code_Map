06902c31823a00f8d5eb716e3affafa163a6fc18
Sarah Fry <sfry06@yahoo.com>
2024-08-27 11:05:16 -0700

    add prettier

--- twilio_map/src/App.js
+++ twilio_map/src/App.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import AreaCodeFeatures from "./components/prefix_boundaries";
-import PrefixModal from "./components/prefix_modal";
-import PrefixSearch from "./components/prefix_search";
-import Searchdropdown from "./components/search_drop_down";
-import CreateMarker from "./components/createMarker";
-import ResetButton from "./components/reset_button";
-import BlueTwiliologo from "./components/blueLogo.png";
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import AreaCodeFeatures from './components/prefix_boundaries';
+import PrefixModal from './components/prefix_modal';
+import PrefixSearch from './components/prefix_search';
+import Searchdropdown from './components/search_drop_down';
+import CreateMarker from './components/createMarker';
+import ResetButton from './components/reset_button';
+import BlueTwiliologo from './components/blueLogo.png';
 
 /* TODO:
    - Add ability to search by prefix and city. When searched, it will fly to the location.
@@ -37,130 +37,140 @@ import BlueTwiliologo from "./components/blueLogo.png";
 */
 
 function App() {
-  const [showModal, setShowModal] = useState(0);
-  const [prefixArray, setPrefixArray] = useState([]);
-  const [prefixSearchArray, setPrefixSearchArray] = useState([
-    { id: 0, label: "loading" },
-  ]);
-  const [searchmarkerCoordinates, setsearchmarkerCoordinates] = useState([]);
-  const [queryType, setqueryType] = useState("prefix");
-  console.log(prefixSearchArray, "prefixARray");
-  const modalShow = (subarray) => {
-    setShowModal((prevShowModal) => prevShowModal + 1);
-    setPrefixArray(subarray);
-  };
+    const [showModal, setShowModal] = useState(0);
+    const [prefixArray, setPrefixArray] = useState([]);
+    const [prefixSearchArray, setPrefixSearchArray] = useState([
+        { id: 0, label: 'loading' },
+    ]);
+    const [searchmarkerCoordinates, setsearchmarkerCoordinates] = useState([]);
+    const [queryType, setqueryType] = useState('prefix');
+    console.log(prefixSearchArray, 'prefixARray');
+    const modalShow = (subarray) => {
+        setShowModal((prevShowModal) => prevShowModal + 1);
+        setPrefixArray(subarray);
+    };
 
-  const prefixCallback = (prefixData) => {
-    console.log(prefixData, "hello");
-    setPrefixSearchArray(prefixData);
-  };
+    const prefixCallback = (prefixData) => {
+        console.log(prefixData, 'hello');
+        setPrefixSearchArray(prefixData);
+    };
 
-  const coordinatesCallback = (coordinates) => {
-    console.log(coordinates, "coordantesarrayysfksldfknsjkldfnskj");
-    setsearchmarkerCoordinates(coordinates);
-  };
+    const coordinatesCallback = (coordinates) => {
+        console.log(coordinates, 'coordantesarrayysfksldfknsjkldfnskj');
+        setsearchmarkerCoordinates(coordinates);
+    };
 
-  const handlesearchType = (searchType) => {
-    console.log(searchType);
-    setqueryType(searchType);
-  };
+    const handlesearchType = (searchType) => {
+        console.log(searchType);
+        setqueryType(searchType);
+    };
 
-  return (
-    <div style={{ height: "100vh" }}>
-      <div
-        style={{
-          height: "70px",
-          position: "absolute",
-          zIndex: 999,
-          top: "0px",
-          left: "0px",
-          width: "100%", // Adjust width as needed
-          backgroundColor: "#F5F5F5",
-          border: "1px solid #A9A9A9",
-        }}
-      >
-        <div
-          style={{
-            left: "0px",
-            marginTop: "0px",
-            width: "150px",
-            position: "relative",
-            zIndex: 1004,
-          }}
-        >
-          <img style={{ width: "100%" }} src={BlueTwiliologo} alt="PIC" />
+    return (
+        <div style={{ height: '100vh' }}>
+            <div
+                style={{
+                    height: '70px',
+                    position: 'absolute',
+                    zIndex: 999,
+                    top: '0px',
+                    left: '0px',
+                    width: '100%', // Adjust width as needed
+                    backgroundColor: '#F5F5F5',
+                    border: '1px solid #A9A9A9',
+                }}
+            >
+                <div
+                    style={{
+                        left: '0px',
+                        marginTop: '0px',
+                        width: '150px',
+                        position: 'relative',
+                        zIndex: 1004,
+                    }}
+                >
+                    <img
+                        style={{ width: '100%' }}
+                        src={BlueTwiliologo}
+                        alt="PIC"
+                    />
+                </div>
+                <div
+                    style={{
+                        zIndex: 1004,
+
+                        position: 'absolute',
+                        top: '0px',
+                        left: '50%',
+                        width: '500px', // Adjust width as needed
+                        height: '100px', // Adjust height as needed
+                        transform: 'translateX(-50%)', // Center the element horizontally
+                        justifyContent: 'center',
+                        display: 'flex',
+                    }}
+                >
+                    <div
+                        style={{
+                            marginTop: '7px',
+                            width: '300px',
+                            maxHeight: '50px',
+                        }}
+                    >
+                        <PrefixSearch
+                            searchType={queryType}
+                            coordinatesCallback={coordinatesCallback}
+                            prefixData={prefixSearchArray}
+                        />
+                    </div>
+                    <div style={{ marginRight: '10px' }}></div>
+                    <div
+                        style={{
+                            backgroundColor: 'white',
+                            zIndex: 3000,
+                            postition: 'absolute',
+                            marginLeft: '-43px',
+                            marginTop: '23px',
+                            height: '25px',
+                            width: '25px',
+                        }}
+                    >
+                        <Searchdropdown handlesearchType={handlesearchType} />
+                    </div>
+                </div>
+            </div>
+
+            <MapContainer
+                center={[39.5, -98.35]}
+                zoom={5}
+                scrollWheelZoom={true}
+                style={{ height: '100vh', width: '100%' }}
+                zoomControl={false}
+                minZoom={5}
+            >
+                <div
+                    style={{
+                        zIndex: 2000,
+                        position: 'absolute',
+                        right: '25px',
+                        marginTop: '25px',
+                    }}
+                >
+                    <ResetButton />
+                </div>
+
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=197c870678314254ae332bcd6f5661d0"
+                />
+                <AreaCodeFeatures
+                    tileUpdate={searchmarkerCoordinates}
+                    prefixsearchData={prefixCallback}
+                    showModalTrigger={modalShow}
+                />
+                <PrefixModal prefixArray={prefixArray} showModal={showModal} />
+                <CreateMarker coordinates={searchmarkerCoordinates} />
+            </MapContainer>
         </div>
-        <div
-          style={{
-            zIndex: 1004,
-
-            position: "absolute",
-            top: "0px",
-            left: "50%",
-            width: "500px", // Adjust width as needed
-            height: "100px", // Adjust height as needed
-            transform: "translateX(-50%)", // Center the element horizontally
-            justifyContent: "center",
-            display: "flex",
-          }}
-        >
-          <div style={{ marginTop: "7px", width: "300px", maxHeight: "50px" }}>
-            <PrefixSearch
-              searchType={queryType}
-              coordinatesCallback={coordinatesCallback}
-              prefixData={prefixSearchArray}
-            />
-          </div>
-          <div style={{ marginRight: "10px" }}></div>
-          <div
-            style={{
-              backgroundColor: "white",
-              zIndex: 3000,
-              postition: "absolute",
-              marginLeft: "-43px",
-              marginTop: "23px",
-              height: "25px",
-              width: "25px",
-            }}
-          >
-            <Searchdropdown handlesearchType={handlesearchType} />
-          </div>
-        </div>
-      </div>
-
-      <MapContainer
-        center={[39.5, -98.35]}
-        zoom={5}
-        scrollWheelZoom={true}
-        style={{ height: "100vh", width: "100%" }}
-        zoomControl={false}
-        minZoom={5}
-      >
-        <div
-          style={{
-            zIndex: 2000,
-            position: "absolute",
-            right: "25px",
-            marginTop: "25px",
-          }}
-        >
-          <ResetButton />
-        </div>
-
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=197c870678314254ae332bcd6f5661d0"
-        />
-        <AreaCodeFeatures
-          tileUpdate={searchmarkerCoordinates}
-          prefixsearchData={prefixCallback}
-          showModalTrigger={modalShow}
-        />
-        <PrefixModal prefixArray={prefixArray} showModal={showModal} />
-        <CreateMarker coordinates={searchmarkerCoordinates} />
-      </MapContainer>
-    </div>
-  );
+    );
 }
 
 export default App;
